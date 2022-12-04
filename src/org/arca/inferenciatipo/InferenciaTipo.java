235fa90b83b6531d81452e743a9696897092623c
server <wll.rgr@gmail.com>
2022-12-04 11:40:08 -0300

    Enhanced For Loop

--- src/org/arca/inferenciatipo/InferenciaTipo.java
+++ src/org/arca/inferenciatipo/InferenciaTipo.java
@@ -1,5 +1,15 @@
 package org.arca.inferenciatipo;
 
+import java.util.List;
+
 public class InferenciaTipo {
 
+	public static void main(String[] args) {
+
+		var listaFruta = List.of("Banana", "Pera", "Uva", "Maçã", "Salada Mista");
+		for(var fruta: listaFruta) {
+			System.out.println(fruta);
+		}
+		
+	}
 }
